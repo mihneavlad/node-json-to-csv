4a02122548ae2e91ca4a8ca6c3cf432708f1678f
mihneavlad <mihnel@yahoo.com>
2018-09-19 15:40:29 +0200

    Created first .csv file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+
+const pathToFile = 'src/lib/students.json';
+
+fs.readFile(pathToFile, (err, data) => {
+    if (err) throw err;
+
+    const students = JSON.parse(data);
+
+		let tableHeader = Object.keys(students[0]).join(', ');
+
+    let tableLine = students.map(student => (
+        Object.values(student).join(', ')
+    )).join('\n') + '\n\n';
 
 
-const names = process.argv.slice(2);
-
-console.log(process.argv.slice(2))
+    fs.writeFile('src/lib/student-summary.csv', (tableHeader + '\n' + tableLine), err => {
+        if (err) throw err;
+    });
+});
