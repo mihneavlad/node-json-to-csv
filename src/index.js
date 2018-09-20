5e4bce6fc245f9c5aee19a9f4e754b6cc677a23c
mihneavlad <mihnel@yahoo.com>
2018-09-20 10:39:32 +0200

    Version that converts any input argument from any location into a .csv file.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import fs from 'fs';
+import path from 'path';
 
-const pathToFile = 'src/lib/students.json';
 
-fs.readFile(pathToFile, (err, data) => {
+const args = process.argv.slice(2);
+// const pathToFile1 = 'src/lib/mihnea.json'
+// const pathToFile2 = path.parse('src/lib/students.json');
+const pathToFile3 = path.parse(args[0]);
+console.log(pathToFile3);
+
+
+fs.readFile(args[0], (err, data) => {
     if (err) throw err;
 
     const students = JSON.parse(data);
 
-		let tableHeader = Object.keys(students[0]).join(', ');
+    let tableHeader = Object.keys(students[0]).join(', ');
 
     let tableLine = students.map(student => (
         Object.values(student).join(', ')
     )).join('\n') + '\n\n';
 
 
-    fs.writeFile('src/lib/student-summary.csv', (tableHeader + '\n' + tableLine), err => {
+    fs.writeFile(path.join(pathToFile3.dir, pathToFile3.name) + '.csv', (tableHeader + '\n' + tableLine), err => {
         if (err) throw err;
+        console.log('Successfully generated!');
     });
 });
